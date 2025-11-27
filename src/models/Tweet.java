077c2e4502aab570a1ce848a90c82c1311d63699
William Vu <vuktwilliam@gmail.com>
2025-11-26 20:58:52 -0800

    Finished final Tweet and User class design

--- src/models/Tweet.java
+++ src/models/Tweet.java
@@ -1,17 +1,34 @@
 package models;
 import java.time.LocalDateTime;
+import java.util.HashSet;
 
 public class Tweet {
-    private int id;
+    private final int id;
     private User user;
     private LocalDateTime date;
     private String body;
+    private HashSet<User> usersWhoLiked;
 
     public Tweet(int id, User user, LocalDateTime date, String body){
         this.id = id;
         this.user = user;
         this.date = date;
         this.body = body;
+        this.usersWhoLiked = new HashSet<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //override java's addressed-based comparison
+        if (this == o) return true;
+        if (!(o instanceof Tweet)) return false;
+        return id == ((Tweet)o).id;
+    }
+
+    @Override
+    public int hashCode() {
+        //override java's addressed-based hashing
+        return Integer.hashCode(id);
     }
 
     public int getID(){
@@ -30,7 +47,21 @@ public class Tweet {
         return this.body;
     }
 
-    public void editBody(String newBody){
+    public void setBody(String newBody){
         body = newBody;
+    }
+
+    public int getLikeCount(){
+        return usersWhoLiked.size();
+    }
+
+    public void addLike(User account){
+        if(usersWhoLiked.contains(account)) return;
+        usersWhoLiked.add(account);
+    }
+
+    public void removeLike(User account){
+        if(!usersWhoLiked.contains(account)) return;
+        usersWhoLiked.remove(account);
     }
 }
--- src/models/User.java
+++ src/models/User.java
@@ -2,43 +2,67 @@ package models;
 
 import java.util.HashSet;
 import java.util.ArrayList;
-import java.util.TreeSet;
+
+import util.OrderedTweetSet;
 
 public class User{
-    private int id;
+    private final int id;
     private String username;
     private String password;
     private HashSet<User> followers;
     private HashSet<User> following;
     private ArrayList<Tweet> tweets;
-    private TreeSet<Tweet> likes;
-    private TreeSet<Tweet> retweets;
+    private OrderedTweetSet likes;
+    private OrderedTweetSet retweets;
     
-
     public User(int id, String username, String password){
         this.id = id; this.username = username; this.password = password;
         this.followers = new HashSet<>();
         this.following = new HashSet<>();
         this.tweets = new ArrayList<>();
+        this.likes = new OrderedTweetSet();
+        this.retweets = new OrderedTweetSet();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        //override java's address based comparison
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        return id == ((User)o).id;
+    }
+
+    @Override
+    public int hashCode() {
+        //override java's address hashing
+        return Integer.hashCode(id);
     }
 
     public int getID(){return this.id;}
     public String getUsername(){return this.username;}
     public String getPassword(){return this.password;}
-    public HashSet<User> getFollowers(){return this.followers;}
-    public HashSet<User> getFollowing(){return this.following;}
-    public ArrayList<Tweet> getTweets(){return this.tweets;}
+    public HashSet<User> getFollowers(){return new HashSet<>(followers);}
+    public HashSet<User> getFollowing(){return new HashSet<>(following);}
+    public ArrayList<Tweet> getTweets(){return new ArrayList<>(tweets);}
+    public Tweet[] getLikes(){return this.likes.getTweets();}
+    public Tweet[] getRetweets(){return this.retweets.getTweets();}
 
     public void editUsername(String other){
         this.username = other;
     }
+
     public void editPassword(String password){
         this.password = password;
     }
+
     public void follow(User other_account){
+        if(other_account == this) return;
+        if(following.contains(other_account)) return;
+
         following.add(other_account);
         other_account.followers.add(this);
     }
+
     public void unfollow(User other_account){
         if(!this.following.contains(other_account)){
             return;
@@ -46,26 +70,33 @@ public class User{
         this.following.remove(other_account);
         other_account.followers.remove(this);
     }
+
     public void post(Tweet new_tweet){
         if(new_tweet.getUser() != this){
             throw new IllegalArgumentException("Tweet must belong to this user");
         }
         this.tweets.add(new_tweet);
     }
+
     public void like(Tweet tweet){
         if(this.likes.contains(tweet)) return;
-        this.likes.add(tweet);
+        this.likes.addTweet(tweet);
+        tweet.addLike(this);
     }
+
     public void unlike(Tweet tweet){
         if(!this.likes.contains(tweet)) return;
-        this.likes.remove(tweet);
+        this.likes.removeTweet(tweet);
+        tweet.removeLike(this);
     }
+
     public void retweet(Tweet tweet){
         if(this.retweets.contains(tweet)) return;
-        this.retweets.add(tweet);
+        this.retweets.addTweet(tweet);
     }
-    public void unRetweet(Tweet tweet){
+
+    public void unretweet(Tweet tweet){
         if(!this.retweets.contains(tweet)) return;
-        this.retweets.remove(tweet);
+        this.retweets.removeTweet(tweet);
     }
 }
